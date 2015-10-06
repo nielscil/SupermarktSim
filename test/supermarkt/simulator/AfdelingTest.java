7dc08d1204043b8d2456e50fd4d4ec6c0a9b189c
Allard Struiksma <a.struiksma@hotmail.com>
2015-10-06 13:20:54 +0200

    kleine update in afdelingtest (niks bijzonders)

--- test/supermarkt/simulator/AfdelingTest.java
+++ test/supermarkt/simulator/AfdelingTest.java
@@ -120,7 +120,8 @@ public class AfdelingTest
         boolean result = instance.isBemand();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        fail("The test case is a prototype.");
+        
     }
 
     /**
